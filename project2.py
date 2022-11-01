7c860a4a522e0e139db26964e486285af1a105cb
Ishva <67134497+hiishva@users.noreply.github.com>
2022-11-01 12:35:04 -0500

    V3
    - Finished the transaction between the teller and the customer

--- project2.py
+++ project2.py
@@ -1,3 +1,4 @@
+import math
 import threading
 import random
 import time
@@ -16,7 +17,7 @@ class Customers():
 ## Teller Objects ##
 class Tellers():
     def __init__(self, tid):
-        print("teller object created")
+        #print("teller object created")
         self.tid = tid
     def __str__(self):
         return ("Teller " + str(self.tid))
@@ -56,6 +57,7 @@ def goToBank(customer, bankLineQueue,waitForEnter):
 
 ## Teller-customer transactions
 def bankTransactions(teller, waitForManager, waitForSafe, bankLineQueue):
+    #TODO figure out how to continuously until the queue is empty
 
     ## CUSTOMER - TELLER INTRODUCTIONS ##   
     customerT = bankLineQueue.get() # Get a customer from the line
@@ -70,24 +72,41 @@ def bankTransactions(teller, waitForManager, waitForSafe, bankLineQueue):
     waitForTransactionType.release()
 
     ## BEGINNING TRANSACTIONS##
-
+    
+    #The transaction is withdraw
     if(transaction == "withdraw"):
         print(str(teller) + " is handling a withdrawal transaction")
         print(str(teller) + " is going to the manager")
         print(str(teller) + " is asking the manager")
-        waitForManager.acquire()
+        waitForManager.acquire() # get the manager
         print(str(teller) + " is getting the manager's permission")
-        time.sleep(random.uniform(.05, .30))
+        time.sleep(random.uniform(.005, .030)) # waiting for manager's permissions
         print (str(teller) + " got the manager's permission")
-        waitForManager.release()
+        waitForManager.release() # release the manager
+    elif(transaction == "deposit"):  # if the transaction is deposit
+        print (str(teller) + " is handling a deposit transaction")
     
+    ## GO TO THE SAFE ##
+    print(str(teller) + " is going to the safe")
+    waitForSafe.acquire()
+    print(str(teller) + " is in the safe")
+    time.sleep(random.uniform(.01, .05)) #Working in the safe (.01s to 0.05s)
+    print(str(teller) + " is leaving the safe")
+    waitForSafe.release()
+
+    print (str(teller) + " is back from the safe")
+
+    ## FINISH THE TRANSACTIONS ##
+    print(str(teller) + " is finished handling " + str(customerT) + "'s transaction.")
+    waitForTeller.release()
+    print(str(customerT) + " is leaving the bank.")
 
 
 
 
 ## Creating the teller threads
 for i in range(3):
-    print("Making the Tellers")
+    #print("Making the Tellers")
     teller = Tellers(i)
     t = threading.Thread(target=bankTransactions, args=(teller, waitForManager, waitForSafe, bankLineQueue))
     t.start()
@@ -100,6 +119,8 @@ for i in range(5):
     c = threading.Thread(target=goToBank, args=(customer, bankLineQueue, waitForEnter))
     c.start()
     customerList.append(c) #Add to the list of customers
+
+
 
 
 
